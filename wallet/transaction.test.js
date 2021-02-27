c15cbb9249f382f17edc1f907f547e7a1b66c1b7
LYUBINOV <faustko@azet.sk>
2021-02-27 22:24:38 +0100

    55.

--- wallet/transaction.test.js
+++ wallet/transaction.test.js
@@ -44,8 +44,8 @@ describe('Transaction', () => {
          expect(transaction.input.amount).toEqual(senderWallet.balance);
       });
 
-      it('sets the `adress` to the `senderWallet` publicKey', () => {
-         expect(transaction.input.adress).toEqual(senderWallet.publicKey);
+      it('sets the `address` to the `senderWallet` publicKey', () => {
+         expect(transaction.input.address).toEqual(senderWallet.publicKey);
       });
 
       it('signs the input', () => {
@@ -58,4 +58,41 @@ describe('Transaction', () => {
          ).toBe(true);
       });
    });
+
+   describe('validTransaction()', () => {
+      let errorMock;
+
+      beforeEach(() => {
+         errorMock = jest.fn();
+
+         global.console.error = errorMock;
+      });
+
+      describe('when the transaction is valid', () => {
+         it('returns true', () => {
+            expect(Transaction.validTransaction(transaction)).toBe(true);
+         });
+      });
+      
+      describe('when the transaction is invalid', () => {
+         describe('and a transaction outputMap value is invalid', () => {
+            it('returns false and logs an error', () => {
+               transaction.outputMap[senderWallet.publicKey] = 999999;
+
+               expect(Transaction.validTransaction(transaction)).toBe(false);
+               expect(errorMock).toHaveBeenCalled();
+            });
+         });
+         
+         describe('and the transaction input signature is invalid', () => {
+            it('returns false and logs an error', () => {
+               transaction.input.signature = new Wallet().sign('data');
+
+               expect(Transaction.validTransaction(transaction)).toBe(false);
+               expect(errorMock).toHaveBeenCalled();
+            });
+         });
+      });
+   })
+   
 });
