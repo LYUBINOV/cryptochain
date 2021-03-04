d5e323b807817f68a62e61cb61e5a7f2cd1140be
LYUBINOV <faustko@azet.sk>
2021-03-04 21:48:43 +0100

    69.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 const blockchain = new Blockchain();
 const transactionPool = new TransactionPool();
 const wallet = new Wallet();
-const pubsub = new PubSub({ blockchain });
+const pubsub = new PubSub({ blockchain, transactionPool });
 
 const DEFAULT_PORT = 3000;
 const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
@@ -50,18 +50,31 @@ app.post('/api/transact', (req, res) => {
 
    transactionPool.setTransaction(transaction);
 
-   console.log('transactionPool', transactionPool);
+   pubsub.broadcastTransaction(transaction);
 
    res.json({ type: 'success', transaction });
 });
 
-const syncChains = () => {
-   request({ url: `${ROOT_NODE_ADDRESS}/api/blocks`}, (error, response, body) => {
+app.get('/api/transaction-pool-map', (req, res) => {
+   res.json(transactionPool.transactionMap);
+});
+
+const syncWithRootState = () => {
+   request({ url: `${ROOT_NODE_ADDRESS}/api/blocks` }, (error, response, body) => {
       if(!error && (response.statusCode === 200)) {
          const rootChain = JSON.parse(body);
 
          console.log('replace chain on a sync with', rootChain);
          blockchain.replaceChain(rootChain);
+      }
+   });
+
+   request({ url: `${ROOT_NODE_ADDRESS}/api/transaction-pool-map` }, (error, response, body) => {
+      if(!error && (response.statusCode === 200)) {
+         const rootTransactionPoolMap = JSON.parse(body);
+
+         console.log('replace transaction pool map on a sync with', rootTransactionPoolMap);
+         transactionPool.setMap(rootTransactionPoolMap);
       }
    });
 };
@@ -77,6 +90,6 @@ app.listen(PORT, () => {
    console.log(`listening at localhost:${PORT}`);
 
    if(PORT !== DEFAULT_PORT) {
-      syncChains();
+      syncWithRootState();
    }
 });
