f1b26957a928ce1b291ddcc3a402f8d681dd5d50
LYUBINOV <faustko@azet.sk>
2021-03-16 21:07:13 +0100

    97.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
 class App extends React.Component {
+   state = { walletInfo: { } };
+
+   componentDidMount() {
+      fetch('http://localhost:3000/api/wallet-info')
+         .then(response => response.json())
+         .then(json => this.setState({ walletInfo: json}));
+   }
+   
    render() {
+      const { address, balance } = this.state.walletInfo;
+
       return(
          <div>
-            Welcome to the blockchain...
+            <div>Welcome to the blockchain...</div>
+            <div>Address: {address}</div>
+            <div>Balance: {balance}</div>
          </div>
       );
    }   
